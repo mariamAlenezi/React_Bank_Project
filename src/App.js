d4875e8dce9aa56ebaa951c324d741799beef64f
FaiNasserAlkahtani <mariamkalenizi@gmail.com>
2023-10-17 17:38:00 +0300

    fix project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./pages/Home";
+import NavbarDefault from "./pages/NavbarDefault";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/NavbarDefault.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavbarDefault = () => {
+  return <div></div>;
+};
+
+export default NavbarDefault;
